9f0089f81c6a3261bdfcda818f56254101984e31
Adrian Mui <adrian.mui@gmail.com>
2016-08-23 18:55:34 -0400

    search function for distance and name

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -6,12 +6,13 @@ class SearchsController < ApplicationController
      
     @user = current_user
     if dist = params[:query_dist].to_i
+      name = params[:query_name] if params[:query_name]
       if @user.profile
         address = @user.profile.address
         if @user.profile.location
           longitude = @user.profile.location.longitude
           latitude = @user.profile.location.latitude
-          @places_near_me = @factual.table("places-us").geo("$circle" => {"$center" => [latitude, longitude], "$meters" => dist }).rows
+          @places_near_me = @factual.table("places-us").search("#{name}").geo("$circle" => {"$center" => [latitude, longitude], "$meters" => dist }).rows
         end
       end
     end
